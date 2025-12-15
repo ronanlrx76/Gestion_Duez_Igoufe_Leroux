4ee4c7267d924f30440585d71e99913f4b1e9451
Valentin <valentin.igoufe@etud.u-picardie.fr>
2025-12-15 18:02:39 +0100

    caca

--- docker/backend/app/api/views.py
+++ docker/backend/app/api/views.py
@@ -2,27 +2,45 @@ from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Compte
+from .models import *
 from django.contrib.auth.hashers import make_password, check_password
 
 class RegisterView(APIView):
     permission_classes = [AllowAny]
+
     def post(self, request):
-        email = request.data.get('email')
-        mdp = request.data.get('mdp')
+        mdp = request.data.get("mdp")
+        mail = request.data.get("mail")
+        nom = request.data.get("nom")
+        prenom = request.data.get("prenom")
+        date_naissance = request.data.get("date_naissance")
+        role_id = request.data.get("role_id")
 
-        if not email or not mdp:
-            return Response({"error": "Email et mot de passe requis"}, status=status.HTTP_400_BAD_REQUEST)
-        
-        if Compte.objects.filter(email=email).exists():
-            return Response({"error": "Email déjà utilisé"}, status=status.HTTP_400_BAD_REQUEST)
-        
-        mdp_hash = make_password(mdp)
+        if Identifiant.objects.filter(mail=mail).exists():
+            return Response({"error": "Mail déjà utilisé"}, status=400)
 
-        compte = Compte(email=email, mdp=mdp_hash)
-        compte.save()
+        role = Role.objects.get(id_role=role_id)
 
-        return Response({"success": True, "email": compte.email}, status=status.HTTP_201_CREATED)
+        # 1. Créer l'utilisateur
+        user = Utilisateur.objects.create(
+            nom=nom,
+            prenom=prenom,
+            date_naissance=date_naissance,
+            role=role
+        )
+
+        # 2. Créer l'identifiant associé
+        identifiant = Identifiant.objects.create(
+            password_hash=make_password(mdp),
+            mail=mail,
+            utilisateur=user
+        )
+
+        return Response({
+            "success": True,
+            "user_id": user.id,
+            "id": identifiant.id
+        }, status=201)
 
 
 class LoginView(APIView):
@@ -32,12 +50,12 @@ class LoginView(APIView):
         mdp = request.data.get('mdp')
         
         try:
-            compte = Compte.objects.get(email=email)
-        except Compte.DoesNotExist:
+            user = Identifiant.objects.get(mail=email)
+        except Identifiant.DoesNotExist:
             return Response({"error": "Compte non trouvé"}, status=status.HTTP_404_NOT_FOUND)
         
-        if check_password(mdp, compte.mdp):
-            return Response({"success": True, "email": compte.email})
+        if check_password(mdp, user.password_hash):
+            return Response({"success": True, "email": user.mail})
         else:
             return Response({"error": "Mot de passe incorrect"}, status=status.HTTP_401_UNAUTHORIZED)
 
