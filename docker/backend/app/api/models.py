0edb40d840fecc4818d405d4ae29572160d9c10d
Alexis Duez <alexisduez3@gmail.com>
2025-12-15 18:47:10 +0100

    Developpement du /api/register pour l'inscription

--- docker/backend/app/api/models.py
+++ docker/backend/app/api/models.py
@@ -1,9 +1,93 @@
 from django.db import models
 
-class Compte(models.Model):
-    email = models.CharField(max_length=255, unique=True)
-    mdp = models.CharField(max_length=255)
+# 1. Livre
+class Livre(models.Model):
+    titre = models.CharField(max_length=50)
+    auteur = models.CharField(max_length=150)
+    emplacement_image_couverture = models.CharField(max_length=100, unique=True, null=True, blank=True)
+
+    class Meta: 
+        managed = False 
+        db_table = 'livre'
+
+
+# 2. Exemplaire
+class Exemplaire(models.Model):
+    pass
+
+    class Meta: 
+        managed = False 
+        db_table = 'exemplaire'
+
+
+# 3. Role
+class Role(models.Model):
+    id_role = models.AutoField(primary_key=True)
+    libelle_role = models.CharField(max_length=100)
+    emprunt_max = models.PositiveSmallIntegerField()
+
+    class Meta: 
+        managed = False 
+        db_table = 'Role'
+
+
+# 4. Utilisateur
+class Utilisateur(models.Model):
+    id_utilisateur = models.BigAutoField(primary_key=True, auto_created=True)
+    nom = models.CharField(max_length=50)
+    prenom = models.CharField(max_length=50)
+    date_naissance = models.DateField()
+    role = models.ForeignKey(Role, on_delete=models.CASCADE, db_column='id_role')
+
+    class Meta: 
+        managed = False 
+        db_table = 'utilisateur'
+
+
+# 5. Identifiant
+class Identifiant(models.Model):
+    id = models.IntegerField(primary_key=True, auto_created=True)
+    password_hash = models.CharField(max_length=100)
+    mail = models.EmailField(max_length=60, unique=True)
+    utilisateur = models.OneToOneField(Utilisateur, on_delete=models.CASCADE, db_column='id_utilisateur')
+
+    class Meta: 
+        managed = False 
+        db_table = 'identifiant'
+
+
+# 6. Historique Emprunt
+class HistoriqueEmprunt(models.Model):
+    date_emprunt = models.DateField()
+    retour_prevu = models.DateField()
+    date_retour_effectif = models.DateField()
+    utilisateur = models.ForeignKey(Utilisateur, on_delete=models.CASCADE)
+    exemplaire = models.ForeignKey(Exemplaire, on_delete=models.CASCADE)
+
+    class Meta: 
+        managed = False 
+        db_table = 'historiqueEmprunt'
+
+
+# 7. ExemplaireLivres
+class ExemplaireLivres(models.Model):
+    livre = models.ForeignKey(Livre, on_delete=models.CASCADE)
+    exemplaire = models.ForeignKey(Exemplaire, on_delete=models.CASCADE)
+    statut = models.CharField(max_length=50)
+    etat = models.CharField(max_length=50, null=True, blank=True)
 
     class Meta:
-        managed = False        # Django ne touche pas à la table
-        db_table = 'compte'
+        unique_together = ("livre", "exemplaire")
+        managed = False 
+        db_table = 'exemplaireLivres'
+
+# 8. Emprunt (PrÃªt en cours)
+class Emprunt(models.Model):
+    exemplaire = models.OneToOneField(Exemplaire, on_delete=models.CASCADE)
+    date_emprunt = models.DateField()
+    retour_prevu = models.DateField()
+    utilisateur = models.ForeignKey(Utilisateur, on_delete=models.CASCADE)
+
+    class Meta: 
+        managed = False 
+        db_table = 'emprunt'
--- docker/backend/app/api/views.py
+++ docker/backend/app/api/views.py
@@ -2,28 +2,54 @@ from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Compte
+from .models import *
 from django.contrib.auth.hashers import make_password, check_password
 
 class RegisterView(APIView):
     permission_classes = [AllowAny]
+
     def post(self, request):
-        email = request.data.get('email')
-        mdp = request.data.get('mdp')
+        mdp = request.data.get("mdp")
+        mail = request.data.get("mail")
+        nom = request.data.get("nom")
+        prenom = request.data.get("prenom")
+        date_naissance = request.data.get("date_naissance")
+        id_role = request.data.get("id_role")
 
-        if not email or not mdp:
-            return Response({"error": "Email et mot de passe requis"}, status=status.HTTP_400_BAD_REQUEST)
-        
-        if Compte.objects.filter(email=email).exists():
-            return Response({"error": "Email déjà utilisé"}, status=status.HTTP_400_BAD_REQUEST)
-        
-        mdp_hash = make_password(mdp)
+        if mdp is None or mail is None or nom is None or prenom is None or date_naissance is None or id_role is None:
+            return Response(
+                {"error": "all params is required (mdp, mail, nom, prenom, date_naissance, id_role )"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
 
-        compte = Compte(email=email, mdp=mdp_hash)
-        compte.save()
+        if Identifiant.objects.filter(mail=mail).exists():
+            return Response({"error": "Mail déjà utilisé"}, status=400)
 
-        return Response({"success": True, "email": compte.email}, status=status.HTTP_201_CREATED)
+        from django.shortcuts import get_object_or_404
 
+        role = get_object_or_404(Role, id_role=id_role)
+
+
+        # 1. Créer l'utilisateur
+        user = Utilisateur.objects.create(
+            nom=nom,
+            prenom=prenom,
+            date_naissance=date_naissance,
+            role=role
+        )
+
+        # 2. Créer l'identifiant associé
+        identifiant = Identifiant.objects.create(
+            password_hash=make_password(mdp),
+            mail=mail,
+            utilisateur=user
+        )
+
+        return Response({
+            "success": True,
+            "user_id": user.id_utilisateur,
+            "id": identifiant.id
+        }, status=201)
 
 class LoginView(APIView):
     permission_classes = [AllowAny]
@@ -32,12 +58,12 @@ class LoginView(APIView):
         mdp = request.data.get('mdp')
         
         try:
-            compte = Compte.objects.get(email=email)
-        except Compte.DoesNotExist:
-            return Response({"error": "Compte non trouvé"}, status=status.HTTP_404_NOT_FOUND)
+            user = Identifiant.objects.get(email=email)
+        except Identifiant.DoesNotExist:
+            return Response({"error": "Identifiant non trouvé"}, status=status.HTTP_404_NOT_FOUND)
         
-        if check_password(mdp, compte.mdp):
-            return Response({"success": True, "email": compte.email})
+        if check_password(mdp, user.mdp):
+            return Response({"success": True, "email": user.email})
         else:
             return Response({"error": "Mot de passe incorrect"}, status=status.HTTP_401_UNAUTHORIZED)
 
